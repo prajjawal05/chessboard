d3d89ec700dac283d1453697db02c9487ed3ebc1
prajjawal05 <prajjawalagarwal@gmail.com>
2024-06-04 16:13:54 -0400

    add game info and other stuff

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -4,12 +4,26 @@ import styled from "styled-components";
 // I am not very good with CSS so I have to google about it.
 // I am sorry I forgot about CSS in our call.
 
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px;
+  height: 100vh;
+`;
+
+const TextSection = styled.div`
+  width: 300px;
+  padding: 20px;
+  border: 3px solid #3b2d2c;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  background: #f8f1e4;
+`;
+
 const StyledBoard = styled.div`
   display: grid;
   grid-template-columns: repeat(8, 60px);
   grid-template-rows: repeat(8, 60px);
-  border: 3px solid #444; 
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 `;
 
 const StyledSquare = styled.div`
@@ -18,49 +32,86 @@ const StyledSquare = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    border: 1px solid #999;
+    border: 1px solid #8c6d62;
 
-    background: ${({ rowIndex, colIndex }) => ((rowIndex + colIndex) % 2 === 1 ? 'linear-gradient(135deg, #333 0%, #555 100%)' : 'linear-gradient(135deg, #fff 0%, #ddd 100%)')};
+    background: ${({ rowIndex, colIndex }) => ((rowIndex + colIndex) % 2 === 1 ? '#e3c19b' : '#a47e56')};
+`;
+
+const Piece = styled.div`
+  font-size: ${(props) => (props.isKing ? '50px' : '20px')};
+  color: ${(props) => (props.isWhite ? '#fff' : '#000')};
+  text-align: center;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);
 `;
 
 const INITIAL_BOARD = [
-    ['R', 'H', 'B', 'K', 'Q', 'B', 'H', 'R'],
-    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+    ['BR', 'BH', 'BB', 'BK', 'BQ', 'BB', 'BH', 'BR'],
+    ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
     ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
     ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
     ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
     ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
-    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-    ['R', 'H', 'B', 'K', 'Q', 'B', 'H', 'R']
+    ['WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP'],
+    ['WR', 'WH', 'WB', 'WK', 'WQ', 'WB', 'WH', 'WR']
 ];
 
 
-const Square = ({ i, j }) => {
+const Square = ({ rowIndex, colIndex, cell }) => {
+    const isBlack = (rowIndex + colIndex) % 2 === 1;
+    const className = isBlack ? 'black' : 'white';
+    console.log(cell, cell.startsWith('W'));
     return (
-        <div>{`${i}, ${j} `}</div>
-    )
+        <StyledSquare key={`${rowIndex} -${colIndex} `} className={className} rowIndex={rowIndex} colIndex={colIndex}>
+            {cell !== 'E' &&
+                <Piece isKing={cell[1] === 'K'} isWhite={cell[0] == ('W')}>
+                    {cell[1]}
+                </Piece>
+            }
+        </StyledSquare >
+    );
 }
 
 
 
 const Board = () => {
     let [board, updateBoard] = useState(INITIAL_BOARD);
+    const [currentPlayer, updatePlayer] = useState('W');
+    const [selectedPiece, updateSelectedPiece] = useState(null);
 
     return (
-        <StyledBoard>
-            {board.map((row, rowIndex) => {
-                return row.map((cell, colIndex) => {
-                    const isBlack = (rowIndex + colIndex) % 2 === 1;
-                    const className = isBlack ? 'black' : 'white';
-                    return (
-                        <StyledSquare key={`${rowIndex} -${colIndex} `} className={className} rowIndex={rowIndex} colIndex={colIndex}>
-                            {cell !== 'E' && <div className={`piece ${cell.toLowerCase()} `}>{cell}</div>}
-                        </StyledSquare>
-                    );
-                });
-            })}
-        </StyledBoard>
+        <Container>
+            <StyledBoard>
+                {board.map((row, rowIndex) => {
+                    return row.map((cell, colIndex) => {
+                        return (
+                            <Square cell={cell} rowIndex={rowIndex} colIndex={colIndex} />
+                        )
+                    });
+                })}
+            </StyledBoard>
+            <TextSection>
+                <h1>Game Information</h1>
+                <p>Player {`${currentPlayer == 'W' ? 1 : 2}`} to move</p>
+                {selectedPiece &&
+                    <p>Selected Piece is {board[selectedPiece[0]][selectedPiece[1]]}</p>
+                }
+            </TextSection>
+        </Container>
     )
 }
+
+// const Game = () => {
+//     return (
+//         <>
+//             <StyledGame>
+//                 Chess
+//                 <Board />
+
+//             </StyledGame>
+//         </>
+//     )
+// }
+
+
 
 export default Board;
